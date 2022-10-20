9c3d5757bd3ec6f0603db03492c37f9f2ebc0924
Jose David <jose7312000@gmail.com>
2022-10-20 16:13:02 -0500

    Agregando archivos de configuracion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const expreess = require('express');
+const mongoose = require('mongoose');
+const config = require('../config/config')
 const app = expreess();
+require('dotenv').config();
+
 const port = process.env.PORT || 3000;
+
+//Route
+app.get("/api", (req, res) => {
+    res.send("Hola mundo");
+});
+
+//Conexion
+mongoose.connect(config)
+.then(() => console.log("Conexion exitosa"))
+.catch((error) => console.error(error));
+
 
 app.listen(port, ()=> {
     console.log("Corriendo sin problemas")
