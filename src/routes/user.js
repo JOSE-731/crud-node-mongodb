e2a6f080ca03270710452f8caf81498859d20af0
Jose David <jose7312000@gmail.com>
2022-10-21 15:39:33 -0500

    Obteniendo todos los usuarios

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,13 +3,24 @@ const userSchema = require('../models/user');
 
 const router = express.Router();
 
+//Creando usuarios
 router.post('/users', (req, res) => {
     //Con la siguiente linea crearemos un usuario con la estructura de datos definidas en el modelo
     const user = userSchema(req.body);
     user
         .save()
         .then((data) => res.json(data))
-        .catch((error) => res.json({message: error}));
+        .catch((error) => res.status(500).json({ message: error }));
 });
+
+//Obtener todos los usuarios:
+router.get('/users', (req, res) => {
+    //Con la siguiente linea crearemos un usuario con la estructura de datos definidas en el modelo
+    userSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error }));
+});
+
 
 module.exports = router;
