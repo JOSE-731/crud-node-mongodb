b8ab16108ea70c317bf949587d54e672ad16a798
Jose David <jose7312000@gmail.com>
2022-10-21 15:50:21 -0500

    Trayendo un registro

--- src/routes/user.js
+++ src/routes/user.js
@@ -22,5 +22,14 @@ router.get('/users', (req, res) => {
         .catch((error) => res.status(500).json({ message: error }));
 });
 
+//Acceder a un solo usuario
+router.get('/users/:id', (req, res) => {
+    const {id} = req.params;
+    //Con la siguiente linea crearemos un usuario con la estructura de datos definidas en el modelo
+    userSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error }));
+});
 
 module.exports = router;
