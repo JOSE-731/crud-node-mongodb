2172c5eedd728b1c06b0b62340dde8d38f7a0098
Jose David <jose7312000@gmail.com>
2022-10-26 22:17:30 -0500

    Carga de archivos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import config from '../config/config.js';
-import  userRoutes  from './routes/user.js';
+import userRoutes from './routes/user.js';
 
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,12 +19,19 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/v1/', userRoutes);
 
+app.use((req, res) =>{
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+console.log(__dirname)
+
+
 /* This is a middleware that is executed when the route is not found. */
-app.use((req, res, next) =>{
+/*app.use((req, res, next) =>{
     res.status(404).json({
         message: "Ruta no encontrada"
     })
-})
+})*/
 
 //Route
 app.get("/api", (req, res) => {
@@ -26,12 +41,12 @@ app.get("/api", (req, res) => {
 //Conexion
 /* Connecting to the database. */
 mongoose.connect(config)
-.then(() => console.log("Conexion exitosa"))
-.catch((error) => console.error(error));
+    .then(() => console.log("Conexion exitosa"))
+    .catch((error) => console.error(error));
 
 
 /* Listening to the port 3000. */
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log("Corriendo sin problemas")
 })
 
